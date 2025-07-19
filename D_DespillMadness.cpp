b87b8e994d5fe8d0438dff351024bcf401a2327e
Danb106632 <daniel.beeching3@gmail.com>
2025-07-19 22:34:50 +0100

    Adding Knobs
    Added knobs with default values
    
    TODO:
    mask is on by default
    fine tune gui is bugged

--- D_DespillMadness.cpp
+++ D_DespillMadness.cpp
@@ -2,32 +2,93 @@
 // Minimum boilerplate code required to create a Nuke Op/Plugin
  
 // A string to define your class name.
-static const char* const RCLASS = "D_DespillMadness";
+static const char* const CLASS = "D_DespillMadness";
  
 // Includes, in this case we need the NoIop class to inherit from it.
 #include "DDImage/NoIop.h"
+#include "DDImage/Knobs.h"
  
 // Namespace. In this case we don't need it, but it's usually convenient to have.
 using namespace DD::Image;
+
+static const char* const screenTypeKnobNames[]  = { "green", "blue", 0 };
+static const char* const despillAlgorithmKnobNames[]  = { "average", "double green/blue average", "double red average", "red limit", "green/blue average", 0 };
  
 // Our class: MyNoOp, inheriting from NoIop.
 class D_DespillMadness : public NoIop
 {
+    int screenTypeKnobKnob;
+    int despillAlgorithmKnobKnob;
+    float fineTuneKnob;
+    bool restoreSourceLuminanceKnob;
+    float saturationKnob[4];
+    float gammaKnob[4];
+    float offsetKnob[4];
+    bool outputSpillMatteKnob;
+    Channel maskChannel;
+    bool invertMask;
+    float mix;
+
 public:
-    // Constructor
-    D_DespillMadness(Node* node) : NoIop(node) {}
-    // Class member function, must return the class name.
-    const char* Class() const override { return RCLASS; }
-    // Help string, optional, but good to get into the habit of defining it.
+    D_DespillMadness(Node* node) : NoIop(node)
+    {
+        inputs(2);
+        screenTypeKnobKnob = 0;
+        despillAlgorithmKnobKnob = 0;
+        fineTuneKnob = 0.94f;
+        restoreSourceLuminanceKnob = true;
+        outputSpillMatteKnob = false;
+        //maskChannel = Chan_Alpha;
+        invertMask = false;
+        mix = 1.0f;
+
+        for (int i=0; i<4; i++) 
+        {
+            saturationKnob[i] = 1.0f;
+            gammaKnob[i] = 1.0f;
+            offsetKnob[i] = 0.0f;
+        }
+    }
+
+    const char* input_label(int n, char*) const override
+    {
+        switch (n) {
+        case 0: return "img";
+        default: return "mask";
+        }
+    }
+    
+    void D_DespillMadness::_validate(bool for_real) override
+    {
+        NoIop::_validate(for_real);
+    }
+
+    void D_DespillMadness::knobs(Knob_Callback f) override
+    {
+        Enumeration_knob(f, &screenTypeKnobKnob, screenTypeKnobNames, "screenType", "screen type");
+        Enumeration_knob(f, &despillAlgorithmKnobKnob, despillAlgorithmKnobNames, "algorithm", "despill algorithm");
+        Float_knob(f, &fineTuneKnob, IRange(0.5f, 1.5f), "LimitPercentage", "fine tune");
+        Divider(f, "");
+        Text_knob(f, "spill area correction"); SetFlags(f, Knob::STARTLINE);
+        Text_knob(f, " "); SetFlags(f, Knob::STARTLINE); Spacer(f, 250);
+        Bool_knob(f, &restoreSourceLuminanceKnob, "restore source luminance"); Tooltip(f, "Restores luminance of the original image"); SetFlags(f, Knob::STARTLINE);
+        AColor_knob(f, saturationKnob, IRange(0.f, 4), "saturation", "saturation");
+        AColor_knob(f, gammaKnob, IRange(0.2f, 5), "gamma", "gamma");
+        AColor_knob(f, offsetKnob, IRange(-1, 1), "offset", "offset");
+        Divider(f, "");
+        Bool_knob(f, &outputSpillMatteKnob, "output spill matte in alpha"); SetFlags(f, Knob::STARTLINE);
+        Divider(f, "");
+        Input_Channel_knob(f, &maskChannel, 1, 2, "maskChannel", "mask channel");
+        Bool_knob(f, &invertMask, "invertMask", "invert");
+        Float_knob(f, &mix, "mix");
+
+    }
+
+    const char* Class() const override { return CLASS; }
     const char* node_help() const override { return "Do nothing."; }
-    // Define the class description object. At this point it's not initialized yet.
-    static Iop::Description d;
+
+    static const Description d;
 };
  
-// Create a build function. It is responsible for initializing our Op with a Node object, that Nuke will create.
 static Iop* build(Node* node) { return new D_DespillMadness(node); }
-// Initialize our class description. 
-// The first argument MUST be the same object as the return value of our Op's Class() function.
-// The second argument is a leftover from Nuke4, back before nuke menus were made with TCL, then Python. It was representing where the menu should be created. Nuke still expects a value but doesn't use it.
-// The third argument is a pointer to our build function.
-Iop::Description D_DespillMadness::d(RCLASS, "Other/D_DespillMadness", build);
+const Iop::Description D_DespillMadness::d(CLASS, nullptr, build);
